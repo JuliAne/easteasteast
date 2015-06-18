ffddeb87ffb1a1062250b992d6b3542834f7757e
Ruth von Borell <ruthvonborell@Ruths-MacBook-Pro.local>
2015-06-18 19:58:08 +0200

    aktuelles aae_data modul

---
+++ Drupal-Module/aae_data/database/db_config.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+ * Database Connection Variables
+ */
+//Put your DB User Name here
+ define('DB_USER', "ruth");
+ //Put your DB password here
+ define('DB_PASSWORD', "boschemoi");
+ //Put your DB for Drupal in here
+ define('DB_DATABASE', "drupal");
+ //Put your server in here
+ define('DB_SERVER', "localhost");
+?>
