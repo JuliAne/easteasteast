c845103eaab0d27198cddccfd44c1de07817f402
Paul Eisenhuth <eisenhuth451@gmail.com>
2015-06-22 21:18:21 +0200

    Erstes Release zusammengepackt

---
+++ Release/1.0/aae_data/database/db_config.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+ * Database Connection Variables
+ */
+//Put your DB User Name here
+ define('DB_USER', "root");
+ //Put your DB password here
+ define('DB_PASSWORD', "1234");
+ //Put your DB for Drupal in here
+ define('DB_DATABASE', "drupal");
+ //Put your server in here
+ define('DB_SERVER', "localhost");
+?>
