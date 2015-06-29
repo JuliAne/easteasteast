ecc88a769a2d8688ad9d34901ce4d08bf8986a8f
Klemens Schölhorn <klemens@schoelhorn.eu>
2015-06-29 17:50:49 +0200

    Add pcai mysql configuration

--- Drupal-Module/aae_data/database/db_config.php
+++ Drupal-Module/aae_data/database/db_config.php
@@ -4,11 +4,11 @@
  * Database Connection Variables
  */
 //Put your DB User Name here
- define('DB_USER', "ruth");
+ define('DB_USER', "swp15-aae");
  //Put your DB password here
- define('DB_PASSWORD', "boschemoi");
+ define('DB_PASSWORD', "visherit");
  //Put your DB for Drupal in here
- define('DB_DATABASE', "drupal");
+ define('DB_DATABASE', "swp15_aae");
  //Put your server in here
  define('DB_SERVER', "localhost");
 ?>
