acf09f3bbecb549c5621fbbc90e496c794637b92
Felix <felix@artplantation.de>
2015-07-10 13:29:44 +0200

    we

--- Drupal-Themen/aae_theme/templates/page--front.tpl.php
+++ Drupal-Themen/aae_theme/templates/page--front.tpl.php
@@ -31,7 +31,7 @@
 
     <?php
   // Load content_types.inc from the node module.
-  module_load_include('.module', 'aae', 'block_aae_letzte_akteure');
+  module_load_include('.module', 'aae_data', 'block_aae_letzte_akteure');
   print_r(block_aae_letzte_akteure_page());
 
    ?>
