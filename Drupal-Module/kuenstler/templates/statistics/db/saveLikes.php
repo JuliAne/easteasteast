0c740b97ec3d003f213bd2c44a8d6ca6a7160689
Ruth von Borell <ruthvonborell@Ruths-MacBook-Pro.local>
2015-07-13 21:19:45 +0200

    nicht benoetigtes Modul

--- Drupal-Module/kuenstler/templates/statistics/db/saveLikes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/db_connect.php';
-
-$db = new DB_CONNECT();
-
-$query = "UPDATE profile SET AmountLikes = " . $amountLikes;
-mysql_query($query);
-$error = mysql_error();
