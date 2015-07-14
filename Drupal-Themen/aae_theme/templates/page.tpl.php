56dc0a65424e1138ad9792bcbe6c444852f94e44
Felix <felix@artplantation.de>
2015-07-14 15:55:19 +0200

    ...

--- Drupal-Themen/aae_theme/templates/page.tpl.php
+++ Drupal-Themen/aae_theme/templates/page.tpl.php
@@ -1,7 +1,5 @@
 <?php include_once('header.tpl.php'); ?>
 
-<div id="aae-content" class="row">
- <?php print render($page['content']); ?>
-</div>
+<?php print render($page['content']); ?>
 
 <?php include_once('footer.tpl.php'); ?>
