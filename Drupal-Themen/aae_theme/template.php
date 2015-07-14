b866a26da795c9e360425b0db16973b0c3b21bb9
Felix <felix@artplantation.de>
2015-07-14 13:15:51 +0200

    Fix main-nav

--- Drupal-Themen/aae_theme/template.php
+++ Drupal-Themen/aae_theme/template.php
@@ -43,23 +43,10 @@ function aae_preprocess_html(&$variables) {
  }
 }
 
-/* main ul */
-function aae_menu_link(array $variables) {
-  //unset all the classes
-  unset($variables['element']['#attributes']['class']);
 
-  $element = $variables['element'];
-
-  //if($variables['element']['#attributes'])
-
-  $sub_menu = '';
-
-  if ($element['#below']) {
-    $sub_menu = drupal_render($element['#below']);
-  }
-  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
-}
+/**
+* Überschreibt das main-menu
+*/
 
 function aae_process_page(&$variables) {
     $menu_tree = menu_tree_all_data('main-menu');
