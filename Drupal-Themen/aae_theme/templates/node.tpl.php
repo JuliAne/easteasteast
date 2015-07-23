0dc5cb6234e8270f11d7ef174fa49edc2b168028
Felix <felix@artplantation.de>
2015-07-23 21:44:32 +0200

    ...

--- Drupal-Themen/aae_theme/templates/node.tpl.php
+++ Drupal-Themen/aae_theme/templates/node.tpl.php
@@ -1,15 +1,11 @@
 <?php if ($teaser): ?>
+
 <h4><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h4>
 <?= print $image; ?> Autor:<?= print $author; ?>
 
 Print: <br />
-<? print print_r($content); ?>
+<? print($content); ?>
 <br />
 
-<?php   // Remove the "Add new comment" link on the teaser page or if the comment
-  // form is being displayed on the same page.
-  if ($teaser || !empty($content['comments']['comment_form'])) {
-    unset($content['links']['comment']['#links']['comment-add']);
-  } ?>
 
 <?php endif; ?>
