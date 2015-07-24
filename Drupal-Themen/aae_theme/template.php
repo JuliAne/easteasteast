44fc5ae6ce9b3bc164a7807fb0ed922a44315a46
Felix <felix@artplantation.de>
2015-07-24 16:57:29 +0200

    ...

--- Drupal-Themen/aae_theme/template.php
+++ Drupal-Themen/aae_theme/template.php
@@ -49,7 +49,7 @@ function aae_preprocess_html(&$variables) {
 
  global $user;
 
-  if (!array_intersect(array('redakteur','administrator'), $user->roles)) {
+  if (array_intersect(array('redakteur','administrator'), $user->roles)) {
    echo '<!-- IF IS_ADMIN -->';
    echo '<style type="text/css">#mainnav { top: 65px !important; z-index: 20 !important; }</style>';
    echo '<!-- /IF -->';
