4e525cd785738c9666e162b86f7a1df29a60abaf
Felix <felix@artplantation.de>
2015-07-24 18:27:33 +0200

    ..

--- Drupal-Module/aae_data/akteurformular.php
+++ Drupal-Module/aae_data/akteurformular.php
@@ -54,7 +54,7 @@ $gps = "";
 //$kategorie = array();
 
 //Speicherort fuer Bilder
-$bildpfad = "/home/swp15-aae/drupal/images/";
+$bildpfad = "drupal/images/";
 
 //-----------------------------------
 
