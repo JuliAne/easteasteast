c3842187508380fb6e593f10f7017d7e7c38b3a6
Ruth von Borell <ruthvonborell@Ruths-MacBook-Pro.local>
2015-08-12 23:26:19 +0200

    Filterung und Beschreibung

--- Drupal-Module/aae_data/events.php
+++ Drupal-Module/aae_data/events.php
@@ -8,22 +8,57 @@
 //-----------------------------------
 
 $tbl_events = "aae_data_event";
+$tbl_event_tags = "aae_data_event_hat_sparte";
+$tbl_tags = "aae_data_kategorie";
 
 //-----------------------------------
 
 require_once $modulePath . '/database/db_connect.php';
 $db = new DB_CONNECT();
 
-//Auswahl aller Akteure (nur Name) in alphabetischer Reihenfolge
-$resultevents = db_select($tbl_events, 'a')
-  ->fields('a', array(
-    'name',
-    'EID',
+$pathThisFile = $_SERVER['REQUEST_URI']; 
+
+$resulttags = db_select($tbl_tags, 't')
+  ->fields('t', array(
+    'KID',
+	'kategorie',
   ))
-  ->orderBy('name', 'ASC')
   ->execute();
+$counttags = $resulttags->rowCount();
 
 //-----------------------------------
+if (isset($_POST['submit'])) {
+  $tag = $_POST['tag'];
+  if($tag != 0){
+    //Auswahl der Events mit entsprechendem Tag in alphabetischer Reihenfolge
+    $result = db_select($tbl_event_tags, 't');
+    $result->join($tbl_events, 'e', 't.hat_EID = e.EID AND t.hat_KID = :kid', array(':kid' => $tag));
+    $result->fields('e', array('name', 'EID', 'kurzbeschreibung', 'start'))->orderBy('name', 'ASC');
+    $resultevents = $result->execute();
+  }else{
+	//Auswahl aller Events in alphabetischer Reihenfolge
+    $resultevents = db_select($tbl_events, 'a')
+    ->fields('a', array(	
+      'name',
+      'EID',
+	  'kurzbeschreibung',
+	  'start',
+	))
+    ->orderBy('name', 'ASC')	
+    ->execute();
+  }
+}else{
+  //Auswahl aller Events in alphabetischer Reihenfolge
+  $resultevents = db_select($tbl_events, 'a')
+    ->fields('a', array(
+      'name',
+      'EID',
+	  'kurzbeschreibung',
+	  'start',
+    ))
+    ->orderBy('name', 'ASC')
+    ->execute();
+}
 
 //Ausgabe
 $profileHTML = <<<EOF
@@ -34,7 +69,23 @@ if(user_is_logged_in()){//Link für Generierung eines neuen Akteurs anzeigen
   $profileHTML .= '<a href="?q=Eventformular">Neue Veranstaltung hinzufügen!</a><br><br>';
 }
 
+$profileHTML .= <<<EOF
+<form action='$pathThisFile' method='POST' enctype='multipart/form-data'>
+EOF;
+//Auswahl eines Tags
+$profileHTML .= '<select name="tag" size="'.$counttags.'" >';
+$profileHTML .= '<option value="0" selected="selected" >keine Auswahl</option>';
+foreach ($resulttags as $row) {
+  $profileHTML .= '<option value="'.$row->KID.'">'.$row->kategorie.'</option>';
+}
+$profileHTML .= '</select>';
+$profileHTML .= '<input type="submit" class="event" id="eventSubmit" name="submit" value="OK">';
+
+$profileHTML .= <<<EOF
+</form>
+EOF;
+
+//Ausgabe der Events
 foreach($resultevents as $row){
-  //$profileHTML .= '<p>'.$row->name.'</p>';
-  $profileHTML .= '<a href="?q=Eventprofil/'.$row->EID.'">'.$row->name.'</a><br>';
+  $profileHTML .= '<p><a href="?q=Eventprofil/'.$row->EID.'">'.$row->name.'</a>: '.$row->kurzbeschreibung.'</p><br>';
 }
