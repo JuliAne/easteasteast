4bb2f058e2981f9250e7802ecf2f7937408a977d
Felix <felix@artplantation.de>
2015-09-02 20:02:44 +0200

    ..

--- Drupal-Themen/aae_theme/js/eventform.js
+++ Drupal-Themen/aae_theme/js/eventform.js
@@ -8,9 +8,8 @@ $(document).ready(function(){
       url: "https://api.mapbox.com/v4/geocode/mapbox.places/1600+pennsylvania+ave+nw.json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
     })
       .done(function( data ) {
-        if ( console && console.log ) {
-          console.log( "Sample of data:", data.slice( 0, 100 ) );
-        }
+          console.log( "Sample of data:", data );
+
       });
   });
 
