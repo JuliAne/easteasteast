3d2dc53d366e441ab4bf7fcfaf9a8dc026abbfce
Felix <felix@artplantation.de>
2015-09-02 20:15:25 +0200

    ..

--- Drupal-Themen/aae_theme/js/eventform.js
+++ Drupal-Themen/aae_theme/js/eventform.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $('#eventStrasseInput').click(function(){
 
     $.ajax({
-      url: "https://api.mapbox.com/v4/geocode/mapbox.places/Neuhofer+Strasse.json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
+      url: "https://api.mapbox.com/v4/geocode/mapbox.places/04347+Leipzig+Ossietzkystrasse+19.json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
     })
       .done(function( data ) {
           console.log( "Sample of data:", data.features[0].center );
