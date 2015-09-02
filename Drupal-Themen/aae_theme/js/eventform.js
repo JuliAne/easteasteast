1d4169f852a72642ee033ad3b8f46c49c73fc5f9
Felix <felix@artplantation.de>
2015-09-02 20:08:19 +0200

    ..

--- Drupal-Themen/aae_theme/js/eventform.js
+++ Drupal-Themen/aae_theme/js/eventform.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
   $('#eventStrasseInput').click(function(){
 
     $.ajax({
-      url: "https://api.mapbox.com/v4/geocode/mapbox.places/1600+pennsylvania+ave+nw.json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
+      url: "https://api.mapbox.com/v4/geocode/mapbox.places/Neuhofer+Strasse.json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
     })
       .done(function( data ) {
-          console.log( "Sample of data:", data[0] );
+          console.log( "Sample of data:", data );
 
       });
   });
