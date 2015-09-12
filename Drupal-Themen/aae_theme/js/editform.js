18fe1aec6a992be938301297ba6dc0ce2aa9a9cc
Felix <felix@artplantation.de>
2015-09-12 19:31:13 +0200

    ...

--- Drupal-Themen/aae_theme/js/editform.js
+++ Drupal-Themen/aae_theme/js/editform.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $("#eventSpartenInput").tokenInput("?q=ajax");
 
   $("#eventStartdatumInput").Zebra_DatePicker({ 'readonly_element' : false });
-  $("#eventEndedatumInput").Zebra_DatePicker({ 'readonly_element' : false });
+  $("#eventEnddatumInput").Zebra_DatePicker({ 'readonly_element' : false });
 
   $('.eventAdresse').click(function(){
 
