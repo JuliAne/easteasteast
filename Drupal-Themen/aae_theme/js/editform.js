e2169f40b1a91fc72ff14cf9f7ef0c328e22a5a2
Felix <felix@artplantation.de>
2015-09-12 19:27:57 +0200

    ...

---
+++ Drupal-Themen/aae_theme/js/editform.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  $("#eventSpartenInput").tokenInput("?q=ajax");
+
+  $("eventStartdatumInput").Zebra_DatePicker();
+
+  $('.eventAdresse').click(function(){
+
+    if ($('#eventNrInput').val() != '' && $('#eventPLZInput').val() != '' && $('#eventStrasseInput').val() != '') {
+
+    $.ajax({
+      url: "https://api.mapbox.com/v4/geocode/mapbox.places/"+ $('#eventPLZInput').val() +"Leipzig+" + $('#eventStrasseInput').val() + "+"+ $('#eventNrInput').val() +".json?access_token=pk.eyJ1IjoibWF0emVsb3QiLCJhIjoiM3JrY3dQTSJ9.IGSonCNVbK5UzSYoxrgMjg",
+    })
+    .done(function( data ) {
+      console.log( "Ermittelte Geo-Koordinaten", data.features[0].center );
+
+      $('#eventGPSInput').val(data.features[0].center[1] + ',' + data.features[0].center[0]);
+
+      });
+    }
+  });
+});
