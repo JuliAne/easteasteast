ba121034a8a8ffa442eeba74f609b358de505f05
Christian <christian-seidemann@mail.de>
2015-09-12 20:02:14 +0200

    Kalender

--- Drupal-Module/aae_data/tag.php
+++ Drupal-Module/aae_data/tag.php
@@ -4,29 +4,98 @@
 	$db = new DB_CONNECT();
 	global $user;
 
+$pathThisFile = $_SERVER['REQUEST_URI']; 
+
 	//EID holen:
 	$path = current_path();
 	$explodedpath = explode("/", $path);
 	$tag = $explodedpath[1];
 	$tbl_event = "aae_data_event";//---r
-	//DB-Abfrage aller Events, die an diesem Tag stattfinden---r
+	$tbl_adresse = "aae_data_adresse";
+	//DB-Abfrage aller Events, die an diesem Tag stattfinden
 	$resultEvents = db_select($tbl_event, 'e')
 	  ->fields('e', array( 
 		'start',
+		'ende',
 		'name',
 		'EID', 
 	  ))
-	  //->condition('start', db_like($this->currentDate.'%'), 'LIKE')
 	  ->condition('start', $tag.'%', 'LIKE')
 	  ->orderBy('name', 'ASC')
 	  ->execute(); 
 	foreach ($resultEvents as $row) {
-		$events .= '<a href="?q=Eventprofil/'.$row->EID.'">'.$row->name.'</a><br>';	
-		//$cellContent .= 'uiuiui';
+		$events .= $row->start.' - '.$row->ende.' '.'<a href="?q=Eventprofil/'.$row->EID.'">'.$row->name.'</a><form action='.$pathThisFile.' method="POST" enctype="multipart/form-data"><input type="hidden" name="eventid" value="'.$row->EID.'"><input type="submit" class="event" id="icalSubmit" name="submit" value="Download"></form><br>';
+	}
+
+if (isset($_POST['submit'])) {
+	$var = null;
+	$event=null;
+	$var .= "BEGIN:VCALENDAR\r\n";
+	$var .= "PRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN\r\n";
+	$var .= "CALSCALE:GREGORIAN\n";
+	$var .= "VERSION:2.0\n";
+	
+	$var .= "BEGIN:VEVENT\n";
+	
+	$eventID = $_POST['eventid'];
+	$resultEvent = db_select($tbl_event, 'e')
+	  ->fields('e', array( 
+		'start',
+		'ende',
+		'name',
+		'EID', 
+		'ort',
+	  ))
+	  ->condition('EID', $eventID, '=')
+	  ->execute(); 
+	
+	foreach ($resultEvent as $row) {
+		$start = $row->start;
+		$ende = $row->ende;
+		$event = $row->name;
+		$ort = $row->ort;
+		$eid = $row->EID;
 	}
 	
+	$resultAdresse = db_select($tbl_adresse, 'a')
+	  ->fields('a', array(
+	    'strasse',
+		'nr',
+		'adresszusatz',
+		'plz',
+	  ))
+	  ->condition('ADID', $ort, "=")
+	  ->execute();
+	
+	$var .= "UID:".makeiCalFormat($start).makeiCalFormat($ende).$eid."@leipziger-ecken.de\n";
+	$var.="DTSTART:".makeiCalFormat($start)."\n";
+	$var.="DTEND:".makeiCalFormat($ende)."\n";
+	$var.="SUMMARY:".$event."\n";
+	foreach ($resultAdresse as $row) {
+		$ad = $row->strasse.' '.$row->nr.'\; '.$row->plz.' Leipzig';
+	}
+	$var.= "LOCATION:".$ad."\n";
+	$var .= "END:VEVENT\n";
+	
+	    header('Content-Type: text/calendar');
+	    header('Content-Length: ' . strlen($var));
+	    header('Content-Disposition: attachment; filename="'.$event.'.ics"');
+	print $var;	
+}
+
+function makeiCalFormat($datum) {
+	//yyyy-mm-dd hh:mm DB (ist)
+	//yyyymmddThhmmss iCal (soll)
+	$datum = str_replace(" ", "T", $datum);
+	$datum = str_replace("-", "", $datum);
+	$datum = str_replace(":", "", $datum);
+	$datum .= "00";
+	return $datum;
+}
+
 
 $profileHTML = <<<EOF
 <p>$events</p>
 
 EOF;
+
