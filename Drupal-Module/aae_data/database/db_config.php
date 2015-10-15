8c5fe728e0e279ec896bd9bf4f39293c7be12fc3
JuliAne <juliane.doehler@googlemail.com>
2015-10-15 21:43:52 +0200

    database config

--- Drupal-Module/aae_data/database/db_config.php
+++ Drupal-Module/aae_data/database/db_config.php
@@ -4,11 +4,11 @@
  * Database Connection Variables
  */
 //Put your DB User Name here
- define('DB_USER', "###111");
+ define('DB_USER', "swp15-aae");
  //Put your DB password here
- define('DB_PASSWORD', "###");
+ define('DB_PASSWORD', "visherit");
  //Put your DB for Drupal in here
- define('DB_DATABASE', "###");
+ define('DB_DATABASE', "swp15_aae");
  //Put your server in here
- define('DB_SERVER', "###");
+ define('DB_SERVER', "localhost");
 ?>
