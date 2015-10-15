d5802ae9cc6bd3337026314f12877dbd4d67b8cc
JuliAne <juliane.doehler@googlemail.com>
2015-10-15 20:26:47 +0200

    updated gitignore, renamed folder LeipzigerEcken

--- Drupal-Module/aae_data/database/db_config.php
+++ Drupal-Module/aae_data/database/db_config.php
@@ -4,11 +4,11 @@
  * Database Connection Variables
  */
 //Put your DB User Name here
- define('DB_USER', "swp15-aae");
+ define('DB_USER', "###");
  //Put your DB password here
- define('DB_PASSWORD', "visherit");
+ define('DB_PASSWORD', "###");
  //Put your DB for Drupal in here
- define('DB_DATABASE', "swp15_aae");
+ define('DB_DATABASE', "###");
  //Put your server in here
- define('DB_SERVER', "localhost");
+ define('DB_SERVER', "###");
 ?>
