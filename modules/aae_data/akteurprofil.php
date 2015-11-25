259613daf898f5f06bcdba36fb21d6ffac91c14d
Felix <felix@artplantation.de>
2015-11-26 00:15:06 +0100

    Strukturierung nach Klassen, Sicherheitserweiterung

--- modules/aae_data/akteurprofil.php
+++ modules/aae_data/akteurprofil.php
@@ -3,59 +3,45 @@
  * Zeigt das Profil eines Akteurs an.
  */
 
-//-----------------------------------
+class aae_akteurprofil extends aae_data_helper {
 
-$tbl_akteur = "aae_data_akteur";
-$tbl_adresse = "aae_data_adresse";
-$tbl_hat_user = "aae_data_akteur_hat_user";
-$tbl_event = "aae_data_event";
-$tbl_akteur_hat_events = "aae_data_akteur_hat_event";
-$tbl_sparte = "aae_data_sparte";
-$tbl_akteur_hat_sparte = "aae_data_akteur_hat_sparte";
+ public function run(){
 
-//-----------------------------------
+  $explodedpath = explode("/", current_path());
+  $akteur_id = $this->clearContent($explodedpath[1]);
 
-//AID holen
-$explodedpath = explode("/", current_path());
-$akteur_id = $explodedpath[1];
+  global $user;
+  $user_id = $user->uid;
 
-//UID holen (ist in user gespeichert)
-global $user;
-$user_id = $user->uid;
-
-//Prüfen ob Schreibrecht vorliegt
-$resultUser = db_select($tbl_hat_user, 'u')
+  //Prüfen ob Schreibrecht vorliegt
+  $resultUser = db_select($this->tbl_hat_user, 'u')
   ->fields('u')
   ->condition('hat_AID', $akteur_id, '=')
   ->condition('hat_UID', $user_id, '=')
   ->execute();
 
-// Anzeige Edit-Button?
-$hat_recht = $resultUser->rowCount();
+  // Anzeige Edit-Button?
+  $hat_recht = $resultUser->rowCount();
 
-//Auswahl der Daten des Akteurs
-$resultakteur = db_select($tbl_akteur, 'a')
-  ->fields('a')
-  ->condition('AID', $akteur_id, '=')
-  ->execute()
-  ->fetchAll();
+  //Auswahl der Daten des Akteurs
+  $resultakteur = db_select($this->tbl_akteur, 'a')
+   ->fields('a')
+   ->condition('AID', $akteur_id, '=')
+   ->execute()
+   ->fetchAll();
 
-if (empty($resultakteur)) {
+  if (empty($resultakteur)) {
   // Akteur nicht vorhanden, beame ihn zur Akteure-Seite
 
-  if (session_status() == PHP_SESSION_NONE) session_start();
+   if (session_status() == PHP_SESSION_NONE) session_start();
+   $_SESSION['sysmsg'][] = 'Dieses Akteurprofil konnte nicht gefunden werden...';
+   header("Location: ".$base_path."/Akteure");
+  }
 
-  $_SESSION['sysmsg'][] = 'Dieses Akteurprofil konnte nicht gefunden werden...';
-
-  header("Location: ".$base_path."/Akteure");
-}
-
-//-----------------------------------
-
-foreach ($resultakteur as $row) {
+ foreach ($resultakteur as $row) {
 
   $aResult['row1'] = $row;
-  $resultAdresse = db_select($tbl_adresse, 'b')
+  $resultAdresse = db_select($this->tbl_adresse, 'b')
    ->fields('b')
 	 ->condition('ADID', $row->adresse, '=')
 	 ->execute()
@@ -67,36 +53,31 @@ foreach ($resultakteur as $row) {
 
 }
 
-// Ziehe Informationen über Events vom Akteur
-$events = db_select($tbl_akteur_hat_events, 'ae')
+  // Ziehe Informationen über Events vom Akteur
+  $events = db_select($this->tbl_akteur_events, 'ae')
   ->fields('ae')
   ->condition('AID', $akteur_id, '=')
   ->execute()
   ->fetchAll();
 
-foreach ($events as $event) {
+  foreach ($events as $event) {
 
- $aResult['events'][] = db_select($tbl_event, 'e')
-  ->fields('e', array(
-   'EID',
-   'name',
-   'kurzbeschreibung',
-   'start',
-   'ende'
-  ))
-  ->condition('EID', $event->EID, '=')
-  ->execute()
-  ->fetchAll();
-}
+   $aResult['events'][] = db_select($this->tbl_event, 'e')
+   ->fields('e')
+   ->condition('EID', $event->EID, '=')
+   ->execute()
+   ->fetchAll();
 
-// Generiere Mapbox-taugliche Koordinaten, übergebe diese ans Frontend
-if ($aResult['row2']->gps != '') {
-  $kHelper = explode(' ', $aResult['row2']->gps, 2);
-  $koordinaten = $kHelper[1] . ',' . $kHelper[0];
+  }
 
-  drupal_add_js('var map = L.mapbox.map("map", "matzelot.mn92ib5i").setView([' . $koordinaten . '], 16);', array('type' => 'inline', 'scope' => 'footer'));
+  // Generiere Mapbox-taugliche Koordinaten, übergebe diese ans Frontend
+  if ($aResult['row2']->gps != '') {
+   $kHelper = explode(' ', $aResult['row2']->gps, 2);
+   $koordinaten = $kHelper[1] . ',' . $kHelper[0];
 
-  // Marker
+   drupal_add_js('var map = L.mapbox.map("map", "matzelot.mn92ib5i").setView([' . $koordinaten . '], 16);', array('type' => 'inline', 'scope' => 'footer'));
+
+   // Marker
   drupal_add_js('L.mapbox.featureLayer({
     type: "Feature",
     geometry: {
@@ -109,28 +90,33 @@ if ($aResult['row2']->gps != '') {
       "marker-size": "large",
       "marker-color": "#1087bf"
     }
-  }).addTo(map);', array('type' => 'inline', 'scope' => 'footer'));
-}
+   }).addTo(map);', array('type' => 'inline', 'scope' => 'footer'));
+  }
 
-$kategorien = db_select($tbl_akteur_hat_sparte, 'a')
- ->fields('a', array('hat_KID'))
- ->condition('hat_AID', $akteur_id, '=')
- ->execute()
- ->fetchAll();
-
-if (!empty($kategorien)) {
-
- foreach($kategorien as $kategorie) {
-
-  $resulttags[] = db_select($tbl_sparte, 't')
-  ->fields('t')
-  ->condition('KID', $kategorie->hat_KID, '=')
+  $kategorien = db_select($this->tbl_hat_sparte, 'a')
+  ->fields('a', array('hat_KID'))
+  ->condition('hat_AID', $akteur_id, '=')
   ->execute()
   ->fetchAll();
 
- }
-}
+  if (!empty($kategorien)) {
 
-ob_start(); // Aktiviert "Render"-modus
-include_once path_to_theme() . '/templates/single_akteur.tpl.php';
-$profileHTML = ob_get_clean(); // Übergebe des gerenderten "single_akteur.tpl"
+  foreach($kategorien as $kategorie) {
+
+   $resulttags[] = db_select($this->tbl_sparte, 't')
+   ->fields('t')
+   ->condition('KID', $kategorie->hat_KID, '=')
+   ->execute()
+   ->fetchAll();
+
+  }
+ }
+
+ ob_start(); // Aktiviert "Render"-modus
+ include_once path_to_theme() . '/templates/single_akteur.tpl.php';
+ return ob_get_clean(); // Übergabe des gerenderten "single_akteur.tpl"
+
+ }
+} // end class aae_akteurprofil
+
+?>
